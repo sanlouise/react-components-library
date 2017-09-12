7be354ca49d6e8b9dcf7af30b03ef930f0fb61b9
sanlouise <sandra.adamshallie@gmail.com>
2017-09-12 09:05:44 -0400

    Update Form Component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,47 +1,24 @@
 // Dependencies
 import React, { Component } from 'react';
-// Externals
-import Input from '../Input';
-import TextArea from '../TextArea'
+import PropTypes from 'prop-types';
+
 
 class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-    };
-  }
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+  };
 
-  updateInput = (field, value) => {
-    this.setState({ [field]: value });
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit();
   }
 
   render() {
     return (
-      <div>
-        {/* Name field */}
-        <Input
-          label="Name"
-          onChange={(event) => this.updateInput('name', event.target.value)}
-          value={this.state.name}
-        />
-        {/* Email field */}
-        <Input
-          label="Email"
-          onChange={(event) => this.updateInput('email', event.target.value)}
-          value={this.state.email}
-        />
-        {/* Message textarea */}
-        <TextArea
-          label="Message"
-          onChange={(event) => this.updateInput('message', event.target.value)}
-          value={this.state.message}
-        />
-        {/* Submit button */}
-        <button onClick={console.log(this.state)}>Submit</button>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        {this.props.children}
+      </form>
     );
   }
 }
